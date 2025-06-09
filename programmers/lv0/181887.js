be9ab5efccbfb7f38fe779a40adf141d9076f50e
Jeein Sophie Han <jeein.han91@gmail.com>
2025-06-09 17:39:46 +0900

    181887 홀수 vs 짝수

--- programmers/lv0/181887.js
+++ programmers/lv0/181887.js
@@ -39,3 +39,12 @@ function solution(num_list) {
     return Math.max(...num_list.reduce(([o, e], c, i) => i % 2 ? [o + c, e] : [o, e + c], [0, 0]));
 }
 // ⭐️
+
+function solution(num_list) {
+    const sum = (condition) => num_list.filter((_, index) => condition(index)).reduce((acc, cur) => acc + cur, 0);
+
+    const oddSum = sum((index) => index % 2 === 0);
+    const evenSum = sum((index) => index % 2 === 1);
+
+    return oddSum < evenSum ? evenSum : oddSum;
+}
